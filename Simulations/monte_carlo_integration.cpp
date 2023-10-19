e11c6656d167e282a30bbc5e1e29f72bb3f34f00
Matthew Liuzza <125320442+Matthew-Liuzza@users.noreply.github.com>
2023-10-19 16:06:21 -0400

    Create monte_carlo_integration.cpp

---
+++ Simulations/monte_carlo_integration.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <random>
+
+// just a simple demonstration of monte_carlo integration
+
+double uniform_x_i(const double a, const double b) {
+    // sample instance from uniform distribution on [a,b]
+
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_real_distribution<double> distr(a,b);
+    return distr(generator);
+}
+
+double x_squared(const double x) {
+    // test function: f(x) = x^2
+    return x * x;
+}
+
+double monte_carlo_integration(const double a, const double b, const double N, double (*func)(double)) {
+    // monte carlo integration -- sum all computed test function for all generated samples x_i from uniform
+    // distribution over [a,b] and multiply by interval length; take sample mean of all these
+
+    double y_i{};
+    for (int i = 0; i < N; ++i)
+        y_i += (b-a) * func(uniform_x_i(a,b));
+    return y_i / N;
+}
+
+
+int main() {
+    // driver code
+
+    constexpr double a{0.0};
+    constexpr double b{10.0};
+    constexpr int N{10000};
+    
+    std::cout << monte_carlo_integration(a,b,N,&x_squared);
+
+    return 0;
+}
