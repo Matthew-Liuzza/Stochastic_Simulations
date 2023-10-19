bde091d15cce5f88c277527fd066814352022547
Matthew Liuzza <125320442+Matthew-Liuzza@users.noreply.github.com>
2023-10-19 18:05:52 -0400

    Update monte_carlo_integration.cpp

--- Simulations/monte_carlo_integration.cpp
+++ Simulations/monte_carlo_integration.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <random>
+#include <cmath>
 
-// just a simple demonstration of monte_carlo integration
+// just a simple demonstration of monte_carlo integration using the standard normal distribution
+constexpr double g_pi{3.141592};
 
 double uniform_x_i(const double a, const double b) {
     // sample instance from uniform distribution on [a,b]
@@ -12,12 +14,12 @@ double uniform_x_i(const double a, const double b) {
     return distr(generator);
 }
 
-double x_squared(const double x) {
-    // test function: f(x) = x^2
-    return x * x;
+double standard_normal(const double x) {
+    // test function: standard normal density
+    return ( 1 / std::sqrt(2 * g_pi) ) * std::exp(-0.5 * x * x);
 }
 
-double monte_carlo_integration(const double a, const double b, const double N, double (*func)(double)) {
+double monte_carlo_integration(const double a, const double b, const double N, double (*func)(const double)) {
     // monte carlo integration -- sum all computed test function for all generated samples x_i from uniform
     // distribution over [a,b] and multiply by interval length; take sample mean of all these
 
@@ -31,11 +33,12 @@ double monte_carlo_integration(const double a, const double b, const double N, d
 int main() {
     // driver code
 
-    constexpr double a{0.0};
-    constexpr double b{10.0};
-    constexpr int N{10000};
+    constexpr double a{-1.0};
+    constexpr double b{1.0};
+    constexpr int N{100000};
     
-    std::cout << monte_carlo_integration(a,b,N,&x_squared);
+    std::cout << monte_carlo_integration(a,b,N,&standard_normal);
 
     return 0;
 }
+
